cd51048584687003360f59541b96b0b818288f27
Gustavo <gustavoquintero9105@gmail.com>
2022-11-13 18:56:37 -0500

    -

--- data.py
+++ data.py
@@ -16,18 +16,9 @@ def F(t,a,b,c):
     aux = 1.0 - np.exp(aux)
     return aux
 
-age = np.empty(29,dtype=int)
-j = 0
-
-for i in range(1,16):
-    age[j] = i
-    j += 1
-
-for i in range(17,36,2):
-    age[j] = i
-    j += 1
-
-age[j:] = [40,45,55,65]
+age = np.array([
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,19,21,23,25,27,29,31,33,35,40,45,55,65
+])
 
 seropositive = np.array([
     0.207,0.301,0.409,0.589,0.757,0.669,0.797,0.818,0.866,0.859,0.908,0.923,0.889,0.936,0.889,\
@@ -40,28 +31,21 @@ t = np.linspace(0,70,1000)
 # sns.boxplot(x=seropositive)
 # plt.show()
 
-free_pos = np.empty(age[-1]-29,dtype=int)
-j = 0
-
-for i in range(1,int(age[-1])+1):
-    if i not in age:
-        free_pos[j] = i
-        j += 1
-
-print(free_pos)
-
-outlier1 = 0.2
-outlier2 = 0.2
-outlier3 = 0.2
+outliers = []
+outliers.append(F(16,*solution) - 0.3)
+outliers.append(F(18,*solution) - 0.3)
+outliers.append(F(20,*solution) - 0.3)
+outliers.append(F(22,*solution) - 0.3)
+outliers.append(F(24,*solution) - 0.3)
 
 age = np.insert(age,15,16)
-seropositive = np.insert(seropositive,15,outlier1)
+seropositive = np.insert(seropositive,15,outliers[0])
 
-age = np.insert(age,26,36)
-seropositive = np.insert(seropositive,26,outlier2)
+# age = np.insert(age,26,36)
+# seropositive = np.insert(seropositive,26,outlier2)
 
-age = np.insert(age,-1,60)
-seropositive = np.insert(seropositive,-1,outlier3)
+# age = np.insert(age,-1,60)
+# seropositive = np.insert(seropositive,-1,outlier3)
 
 samples = len(age)
 
@@ -70,7 +54,7 @@ with open("output/measles.txt","w") as f:
     for i in range(samples):
         f.write("%i %f\n" % (age[i],seropositive[i]))
 
-# plt.plot(age,seropositive,"o")
+plt.plot(age,seropositive,"o")
 # plt.plot(t,F(t,*solution))
 # plt.plot(t,l(t,*solution))
 plt.show()
