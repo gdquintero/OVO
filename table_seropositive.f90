13d56b65ddd002d18fc7f0702f7443f1c23a823f
Gustavo <gustavoquintero9105@gmail.com>
2022-11-16 21:40:41 -0500

    -

--- table_seropositive.f90
+++ table_seropositive.f90
@@ -29,18 +29,19 @@ program table
 
     do i = 1,14
         if (i .ne. 14) then
-            write(100,110) '$[',age(i),',',age(i+1),')$','&',measles(i),'&',mumps(i),'&',rubella(i),&
-                        '$[',age(i+15),',',age(i+16),')$','&',measles(i+15),'&',mumps(i+15),'&',rubella(i+15),'\\'
-            110 format (A2,I2,A1,I2,A2,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,&
-                        A2,I2,A1,I2,A2,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A2)
+            write(100,110) age(i),'$\,-<$',age(i+1),'&',measles(i),'&',mumps(i),'&',rubella(i),'&',&
+                        age(i+15),'$\,-<$',age(i+16),'&',measles(i+15),'&',mumps(i+15),'&',rubella(i+15),'\\'
+            110 format (I2,A6,I2,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,&
+                        I2,A6,I2,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A2)
         else
-            write(100,120) '$[',age(i),',',age(i+1),')$','&',measles(i),'&',mumps(i),'&',rubella(i),'&','&','&','\\'
-            120 format (A2,I2,A1,I2,A2,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,A1,1X,A1,1X,A2)
+            write(100,120) age(i),'$\,-<$',age(i+1),'&',measles(i),'&',mumps(i),'&',rubella(i),'&',&
+                        age(29),'+','&',measles(29),'&',mumps(29),'&',rubella(29),'\\'
+            120 format (I2,A6,I2,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X&
+                        I2,A1,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A2)
         end if
     end do
 
-    ! write(100,120) '$[',age(i),',',age(i+1),')$','&',measles(i),'&',mumps(i),'&',rubella(i),&
-    !                 '$[',age(i+15),',',age(i+16),')$','&',measles(i+15),'&',mumps(i+15),'&',rubella(i+15),'\\'
-    ! 120 format (A2,I2,A1,A7,A2,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A2)
+    write(100,130) age(15),'$\,-<$',age(16),'&',measles(15),'&',mumps(15),'&',rubella(15),'&','&','&','\\'
+    130 format (I2,A6,I2,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,F5.3,1X,A1,1X,A1,1X,A1,1X,A2)
 
 end program
