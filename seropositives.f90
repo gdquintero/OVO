cbafaf923530b272b39193e8ecd48cf096cf723b
Gustavo Quintero <gustavoquintero9105@gmail.com>
2023-02-22 13:46:44 -0500

    Updated on 02-22-2023 at 13:46

--- seropositives.f90
+++ seropositives.f90
@@ -6,7 +6,7 @@ Program main
     integer :: allocerr,samples,q,outliers
     real(kind=8) :: alpha,epsilon,delta,sigmin,fxk,fxtrial,ti,sigma
     real(kind=8), allocatable :: xtrial(:),faux(:),indices(:),nu_l(:),nu_u(:),opt_cond(:),&
-                                 xstar(:),xk(:),grad(:,:),y(:),data(:,:),t(:)
+                                 xstar(:),y(:),data(:,:),t(:)
     integer, allocatable :: Idelta(:)
 
     ! LOCAL SCALARS
@@ -17,7 +17,10 @@ Program main
     ! LOCAL ARRAYS
     character(len=80) :: specfnm,outputfnm,vparam(10)
     logical :: coded(11)
-    real(kind=8),   pointer :: l(:),u(:),x(:)
+    real(kind=8),   pointer :: l(:),u(:),x(:),xk(:),grad(:,:)
+
+    logical,        pointer :: equatn(:),linear(:)
+    real(kind=8),   pointer :: lambda(:)
 
     integer :: i
     real(kind=8), dimension(3,3) :: solutions
@@ -101,11 +104,10 @@ Program main
 
     ! Measles
     y(:) = data(2,:)
-    xk(:) = (/0.197d0,0.287d0,0.021d0/)
-    xk(:) = 1.0d0
+    ! xk(:) = (/0.197d0,0.287d0,0.021d0/)
     delta = 1.0d-2
     sigmin = 1.0d-2
-    call ovo_algorithm(q,delta,sigmin)
+    call ovo_algorithm(q,delta,sigmin,t,y,indices,Idelta,samples,m,n,xtrial)
     print*,"Solution measles: ",xk
     solutions(1,:) = xk(:)
 
@@ -138,26 +140,27 @@ Program main
     !==============================================================================
     ! MAIN ALGORITHM
     !==============================================================================
-    subroutine ovo_algorithm(q,delta,sigmin)
+    subroutine ovo_algorithm(q,delta,sigmin,t,y,indices,Idelta,samples,m,n,xtrial)
         implicit none
 
-        integer,        intent(in) :: q
-        real(kind=8),   intent(in) :: delta, sigmin
-
-        logical,        pointer :: equatn(:),linear(:)
-        real(kind=8),   pointer :: lambda(:)
+        integer,        intent(in) :: q,samples,n
+        real(kind=8),   intent(in) :: delta,sigmin,t(samples),y(samples)
+        integer,        intent(inout) :: Idelta(samples),m
+        real(kind=8),   intent(inout) :: indices(samples),xtrial(n-1)
 
         integer, parameter  :: max_iter = 100000, max_iter_sub = 1000, kflag = 2
         integer             :: iter,iter_sub,i,j
         real(kind=8)        :: gaux1,gaux2,a,b,c,ebt,terminate
 
         iter = 0
+
+        xk(:) = 1.0d0
     
         indices(:) = (/(i, i = 1, samples)/)
     
         ! Scenarios
         do i = 1, samples
-            call fi(xk,i,n,faux(i))
+            call fi(xk,i,n,t,y,samples,faux(i))
         end do
     
         ! Sorting
@@ -166,7 +169,7 @@ Program main
         ! q-Order-Value function 
         fxk = faux(q)
     
-        call mount_Idelta(faux,indices,delta,Idelta,m)
+        call mount_Idelta(faux,delta,indices,samples,Idelta,m)
 
         do
             iter = iter + 1
@@ -191,13 +194,13 @@ Program main
 
                 ebt = exp(-b * ti)
 
-                call model(xk,Idelta(i),n,gaux1)
+                call model(xk,Idelta(i),n,t,samples,gaux1)
 
                 gaux1 = y(Idelta(i)) - gaux1
 
                 gaux2 = exp((a / b) * ti * ebt + (1.0d0 / b) * ((a / b) - c) * (ebt - 1.0d0) - c * ti)
     
-                grad(i,1) = (1.0d0 / b**2) * (ebt - 1.0d0) + (1.0d0 / b) * (ti * ebt)
+                grad(i,1) = (1.0d0 / b**2) * (ebt * (ti * b + 1.0d0) - 1.0d0)
     
                 grad(i,2) = (1.0d0 / b**2) * (c - a/b) * (ebt - 1.0d0)
                 grad(i,2) = grad(i,2) - (1.0d0 / b**3) * (a * (ebt - 1.0d0))
@@ -230,7 +233,7 @@ Program main
     
                 ! Scenarios
                 do i = 1, samples
-                    call fi(xtrial,i,n,faux(i))
+                    call fi(xtrial,i,n,t,y,samples,faux(i))
                 end do
     
                 ! Sorting
@@ -271,45 +274,46 @@ Program main
             ! terminate = norm2(xk - xtrial)
             terminate = norm2(opt_cond)
     
-            ! print*, iter, iter_sub, fxtrial, terminate, m
+            print*, iter, iter_sub, fxtrial, terminate, m
 
             deallocate(lambda,equatn,linear,grad)
 
             fxk = fxtrial
 
             xk(1:n-1) = xtrial(1:n-1)
+
             
             if (terminate .le. epsilon) exit
             if (iter .ge. max_iter) exit
     
-            call mount_Idelta(faux,indices,delta,Idelta,m)
+            call mount_Idelta(faux,delta,indices,samples,Idelta,m)
 
     
         end do ! End of Main Algorithm
         
     end subroutine
 
-    !==============================================================================
-    ! 
-    !==============================================================================
-    subroutine quadatic_error(x,n,res)
-        implicit none 
+    ! !==============================================================================
+    ! ! 
+    ! !==============================================================================
+    ! subroutine quadatic_error(x,n,res)
+    !     implicit none 
 
-        integer,        intent(in) :: n
-        real(kind=8),   intent(in) :: x(n-1)
-        real(kind=8),   intent(out) :: res
-        integer :: i
-        real(kind=8) :: aux
+    !     integer,        intent(in) :: n
+    !     real(kind=8),   intent(in) :: x(n-1)
+    !     real(kind=8),   intent(out) :: res
+    !     integer :: i
+    !     real(kind=8) :: aux
 
-        res = 0.0d0
+    !     res = 0.0d0
 
-        do i = 1, samples
-            call model(x,i,n,aux)
-            aux = aux - y(i)
-            res = res + aux**2
-        enddo
+    !     do i = 1, samples
+    !         call model(x,i,n,aux)
+    !         aux = aux - y(i)
+    !         res = res + aux**2
+    !     enddo
 
-    end subroutine quadatic_error
+    ! end subroutine quadatic_error
 
     !==============================================================================
     ! EXPORT RESULT TO PLOT
@@ -334,9 +338,10 @@ Program main
     !==============================================================================
     ! MOUNT THE SET OF INDICES I(x,delta)
     !==============================================================================
-    subroutine mount_Idelta(f,indices,delta,Idelta,m)
+    subroutine mount_Idelta(f,delta,indices,samples,Idelta,m)
         implicit none
 
+        integer,        intent(in) :: samples
         real(kind=8),   intent(in) :: delta,f(samples),indices(samples)
         integer,        intent(out) :: Idelta(samples),m
         integer :: i
@@ -358,14 +363,14 @@ Program main
     !==============================================================================
     ! QUADRATIC ERROR OF EACH SCENARIO
     !==============================================================================
-    subroutine fi(x,i,n,res)
+    subroutine fi(x,i,n,t,y,samples,res)
         implicit none
 
-        integer,        intent(in) :: n,i
-        real(kind=8),   intent(in) :: x(n-1)
+        integer,        intent(in) :: n,i,samples
+        real(kind=8),   intent(in) :: x(n-1),t(samples),y(samples)
         real(kind=8),   intent(out) :: res
         
-        call model(x,i,n,res)
+        call model(x,i,n,t,samples,res)
         res = res - y(i)
         res = 0.5d0 * (res**2)
 
@@ -374,11 +379,11 @@ Program main
     !==============================================================================
     ! MODEL TO BE FITTED TO THE DATA
     !==============================================================================
-    subroutine model(x,i,n,res)
+    subroutine model(x,i,n,t,samples,res)
         implicit none 
 
-        integer,        intent(in) :: n,i
-        real(kind=8),   intent(in) :: x(n-1)
+        integer,        intent(in) :: n,i,samples
+        real(kind=8),   intent(in) :: x(n-1),t(samples)
         real(kind=8),   intent(out) :: res
         real(kind=8) :: a,b,c,ti,ebt
 
