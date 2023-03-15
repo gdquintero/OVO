dee62b25ca6d9188223bf441f5e6fed679c9abfb
Gustavo <gustavoquintero9105@gmail.com>
2023-03-14 22:07:43 -0500

    Updated on 03-14-2023 at 22:07

--- plot.py
+++ plot.py
@@ -3,11 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import models
 
-def plot_solutions(i,df_seropositives,df_sol_ovo):
+# 'ro',mfc='none',ms=10)
+
+def plot_solutions(i,df_seropositives,df_sol_ovo,sero_outliers):
     t = np.linspace(0,70,1000)
     plt.plot(df_seropositives[0].values,df_seropositives[i].values,"o")
     plt.plot(t,models.F(t,*solutions_farrington[i-1]),label="Farrington")
     plt.plot(t,models.F(t,*df_sol_ovo.iloc[i-1].values),label="OVO")
+    plt.plot(sero_outliers[0],sero_outliers[1],'ro',mfc='none',ms=10)
     plt.legend()
     plt.show()
 
@@ -26,15 +29,29 @@ outliers = np.empty(3*noutliers,dtype=int)
 for i in range(3*noutliers):
     outliers[i] = int(xdata[i+1])
 
+measles_outliers = np.empty((2,noutliers))
+mumps_outliers   = np.empty((2,noutliers))
+rubella_outliers = np.empty((2,noutliers))
+
+for i in range(noutliers):
+    measles_outliers[0,i] = df_seropositives[0].values[outliers[i]-1]
+    measles_outliers[1,i] = df_seropositives[1].values[outliers[i]-1]
+
+    mumps_outliers[0,i] = df_seropositives[0].values[outliers[noutliers+i]-1]
+    mumps_outliers[1,i] = df_seropositives[2].values[outliers[noutliers+i]-1]
+
+    rubella_outliers[0,i] = df_seropositives[0].values[outliers[2*noutliers+i]-1]
+    rubella_outliers[1,i] = df_seropositives[3].values[outliers[2*noutliers+i]-1]
+
+
 solutions_farrington = np.array([
     [0.197,0.287,0.021],
     [0.156,0.250,0.0],
     [0.0628,0.178,0.020]
 ])
 
-
 # Plotamos las soluciones 1:Measles, 2:Mumps, 3:Rubella
-# plot_solutions(1,df_seropositives,df_solutions_ovo)
-# plot_solutions(2,df_seropositives,df_solutions_ovo)
-# plot_solutions(3,df_seropositives,df_solutions_ovo)
+plot_solutions(1,df_seropositives,df_solutions_ovo,measles_outliers)
+plot_solutions(2,df_seropositives,df_solutions_ovo,mumps_outliers)
+plot_solutions(3,df_seropositives,df_solutions_ovo,rubella_outliers)
 
